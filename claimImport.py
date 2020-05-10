37574387928459e4fa50de8c3afbaa9d7b072501
rjrseo@gmail.com <Hapkido123!>
2020-05-10 15:46:30 -0700

    Claim Loop Added

---
+++ claimImport.py
@@ -0,0 +1,8 @@
+class claimClass:
+    def __init__(self):
+        self.rowList = []
+        self.rowDict = {}
+
+
+    def addRow(self, claimRow):
+        pass
--- oa.py
+++ oa.py
@@ -1,6 +1,8 @@
 import fileImport as f
+import claimImport as c
 
 extract = f.fileClass()
+claim = c.claimClass()
 
 extract.openInput("claims1.csv")
 extract.fixAddress("cleaned.csv")
@@ -13,10 +15,9 @@ for i in range(len(claims)):
 
     if claims.iloc[i, 362] == currentAcession:
         x += 1
+        claim.addRow(claims)
         print(x)
     else:
         currentAcession = claims.iloc[i, 362]
         x = 1
         print(x)
-
-
