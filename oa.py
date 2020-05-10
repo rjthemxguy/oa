91a68339b8db4b1063acf56d6d87b3b28f395a4f
rjrseo@gmail.com <Hapkido123!>
2020-05-10 10:15:09 -0700

    Claim Loop Added

--- oa.py
+++ oa.py
@@ -1,9 +1,22 @@
 import fileImport as f
 
-
 extract = f.fileClass()
-
 
 extract.openInput("claims1.csv")
 extract.fixAddress("cleaned.csv")
 claims = extract.get()
+
+currentAcession = claims.iloc[0, 362]
+x = 0
+
+for i in range(len(claims)):
+
+    if claims.iloc[i, 362] == currentAcession:
+        x += 1
+        print(x)
+    else:
+        currentAcession = claims.iloc[i, 362]
+        x = 1
+        print(x)
+
+
